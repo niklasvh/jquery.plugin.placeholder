a03fe72838b7d904a35251329383651f4a488933
Niklas von Hertzen <niklasvh@gmail.com>
2011-05-14 01:38:40 +0300

    initial commit

---
+++ jquery.plugin.placeholder.js
@@ -0,0 +1,46 @@
+/* 
+* jQuery placeholder plugin for non-HTML5 browsers
+* @author Niklas von Hertzen <niklas at hertzen.com>
+* @created 14.5.2011 
+* @website http://hertzen.com
+*/
+
+(function($) {
+    $.fn.placeholder = function(options) {
+                    
+        var defaults = {
+            'placeholderClass':'placeholderFallback',
+            'forceAll':false
+        };
+        
+        var options = $.extend(defaults, options);            
+                    
+        var placeholderText = $(this).attr('placeholder');                
+                
+        
+                  
+        if (!supported()||forceAll){
+            $(this).focusin(function(){
+                if ($(this).val()==placeholderText){
+                    $(this).val('').removeClass(options.placeholderClass);
+                }
+
+            }).focusout(function(){
+                if ($(this).val()==""){
+                    $(this).val(placeholderText).addClass(options.placeholderClass);
+                }             
+            }).trigger('focusout');        
+
+        }
+        
+        /*
+         * Test if users browser supports the placeholder attribute
+         */
+        function supported(){
+            var i = document.createElement('input');
+            return 'placeholder' in i;
+        }
+    }
+})(jQuery);
+
+
